cef1f15127b0a062c3e6b35981af3f07fc0dee3a
kirici <74684021+kirici@users.noreply.github.com>
2023-10-29 13:13:46 +0100

    feat: check for post method before parsing form

--- main.go
+++ main.go
@@ -49,6 +49,10 @@ func newServerFS() *http.Handler {
 }
 
 func handleForm(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	formValues := form.ParseValues(r)
 	writeJSON(formValues, "profile-config.json")
 
