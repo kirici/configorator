d7048d74ae2b827a6f2121b55548234234924412
kirici <74684021+kirici@users.noreply.github.com>
2023-12-23 20:53:02 +0100

    refactor: extract route handlers to funcs

--- main.go
+++ main.go
@@ -23,42 +23,51 @@ func main() {
 	go packer.Fetch()
 
 	// Parse templates during server startup
-	indexTemplate, err := template.ParseFS(content, "templates/index.html", "templates/header.html")
+	indexTpl, err := template.ParseFS(content, "templates/index.html", "templates/header.html")
 	if err != nil {
 		log.Fatalf("Could not parse template: %s", err)
 	}
 
-	submitTemplate, err := template.ParseFS(content, "templates/submit.html", "templates/header.html")
+	submitTpl, err := template.ParseFS(content, "templates/submit.html", "templates/header.html")
 	if err != nil {
 		log.Fatalf("Could not parse template: %s", err)
 	}
 
 	// Requests to "/"
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		err := indexTemplate.Execute(w, nil)
-		if err != nil {
-			log.Fatalf("ERROR: Templates: %s", err)
-		}
-	})
-
+	http.HandleFunc("/", serveIndex(indexTpl))
 	// POST "/submit"
-	http.HandleFunc("/submit", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-			return
-		}
-		err := submitTemplate.Execute(w, nil)
-		if err != nil {
-			log.Fatalf("ERROR: Template error: %s", err)
-		}
-		go packer.Exec(c)
-	})
+	http.HandleFunc("/submit", submitHandler(submitTpl, c))
 
 	// Start the server
 	port := "8080"
 	fmt.Println("Starting server at", port)
 	browser.OpenURL("http://127.0.0.1:" + port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
+}
+
+// Indirection of returning an http Handler to enable passing parameters
+func serveIndex(tpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		err := tpl.Execute(w, nil)
+		if err != nil {
+			log.Fatalf("ERROR: Template error: %s", err)
+		}
+	}
+}
+
+// submitHandler validates the request method and passes the sigterm channel parameter on to the child proc
+func submitHandler(tpl *template.Template, c chan os.Signal) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		err := tpl.Execute(w, nil)
+		if err != nil {
+			log.Fatalf("ERROR: Template error: %s", err)
+		}
+		go packer.Exec(c)
+	}
 }
 
 func trapSIGTERM() chan os.Signal {
